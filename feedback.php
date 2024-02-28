caea0ac7b490e5b913e767b2aa3671ea9c96d51a
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-02-28 18:54:20 +0300

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -8,6 +8,7 @@
  <link rel="stylesheet" href="css/main.css">
  <link  rel="stylesheet" href="css/font.css">
  <script src="js/jquery.js" type="text/javascript"></script>
+	
 
   <script src="js/bootstrap.min.js"  type="text/javascript"></script>
  	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
