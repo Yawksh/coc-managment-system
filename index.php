2797f2c1a98cd61bd8c3a08cb4cf709c0cae9bd8
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-02-28 18:55:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <title>Center of Competence </title>
 >
+
     <link  rel="stylesheet" href="css/bootstrap.min.css"/>
     <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>
     <link rel="stylesheet" href="css/main.css">
