f4807f41ec159f336fa2a215290a3bf69db58186
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-03-01 16:17:12 +0300

    Update forgotepass.php

--- forgotepass.php
+++ forgotepass.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Center of competance </title>
-   
+
+
+	
     <link  rel="stylesheet" href="css/bootstrap.min.css"/>
     <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>
     <link rel="stylesheet" href="css/main.css">
